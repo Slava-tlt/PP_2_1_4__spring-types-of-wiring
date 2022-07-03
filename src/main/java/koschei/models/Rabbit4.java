b986669a272de6dcef7032f35801548d6bb20c24
Slava-tlt <cjiabug1@mail.com>
2022-07-03 11:28:52 +0400

    commit

--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -5,8 +5,11 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Rabbit4 {
-    @Autowired
-    private Duck5 duck;
+    private final Duck5 duck;
+
+    public Rabbit4(Duck5 duck) {
+        this.duck = duck;
+    }
 
     @Override
     public String toString() {return ", в зайце утка " + duck.toString();
